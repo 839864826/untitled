75aa1823f6c894f96533dc0d12ecd3b7e2656e1d
839864826 <839864826@qq.com>
2021-08-27 22:20:48 +0800

    转换流

--- src/IO流/转换流.java
+++ src/IO流/转换流.java
@@ -1,7 +1,6 @@
 package IO流;
 
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /**
  * 处理流之二：转换流的使用
@@ -25,13 +24,56 @@ import java.io.InputStreamReader;
  *  UTF-8：变长的编码方式，可用1-4个字节来表示一个字符。
  *
  *
+ * ● Unicode不完美， 这里就有三个问题，一个是，我们已经知道， 英文字母只用
+ * 一个字节表示就够了，第二个问题是如何才能区别Unicode利ASCII?计算机
+ * 怎么知道两个字节表示-一个符号，而不是分别表示两个符号呢?第三个，如果
+ * 和GBK等双字节编码方式-一样，用最高位是1或0表示两个字节和一个字节，
+ * 就少了很多值无法用于表示字符，不够表示所有字符。Unicode在很长一-段时
+ * 间内无法推广，直到互联网的出现。I
+ * ●面向传输的众多UTF (UCS Transfer Format) 标准出现了，顾名思义，UTF
+ * 8就是每次8个位传输数据，而UTF-16就是每次16个位。这是为传输而设计的
+ * 编码，并使编码无国界，这样就可以显示全世界上所有文化的字符了。
+ * ●Unicode只是定义了一个庞大的、全球通用的字符集，并为每个字符规定了唯
+ * 一确定的编号，具体存储成什么样的字节流，取决于字符编码方案。推荐的
+ * Unicode编码是UTF 8和UTF-16。
+ *
  */
 public class 转换流 {
     //E:\\xunlian\\untitled\\File\\hello.txt
     public static void main(String[] args) {
-        FileInputStream fis = new FileInputStream("File\\hello.txt");
-//        InputStreamReader isr = new InputStreamReader(fis);
-        //
-        InputStreamReader isr8 = new InputStreamReader(fis,"UTF-8");
+        转换流();
+    }
+
+    private static void 转换流() {
+        InputStreamReader isr = null;
+        OutputStreamWriter osw = null;
+        try {
+            FileInputStream fis = new FileInputStream("File\\hello.txt");
+            FileOutputStream fos = new FileOutputStream("File\\hello_gbk.txt");
+
+//            InputStreamReader isr = new InputStreamReader(fis);//默认 idea设置的
+            //File->Settings->Editor->File Encodings
+            isr = new InputStreamReader(fis,"UTF-8");//utf-8
+            osw = new OutputStreamWriter(fos,"gbk");
+
+            char[] chars = new char[20];
+            int len;
+            while ((len = isr.read(chars))!=-1){
+                osw.write(chars,0,len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                isr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                osw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
